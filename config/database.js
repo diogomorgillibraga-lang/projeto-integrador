5c820876762714b4201231dae15d6b5572f5de85
diogomorgillibraga-lang <diogomorgillibraga@gmail.com>
2026-07-03 16:22:14 -0300

    Altera conexao de MySQL para PostgreSQL

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-import mysql from 'mysql2/promise'
+/* import mysql from 'mysql2/promise'
 import dotenv from 'dotenv'
 dotenv.config()
 const pool = mysql.createPool({
@@ -9,6 +9,23 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
+})
+
+export default pool
+ */
+import pg from 'pg'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const { Pool } = pg
+
+// Cria a conexão usando a URL completa do Neon
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    // Exigido pelo Neon para aceitar conexões seguras na nuvem
+    rejectUnauthorized: false
+  }
 })
 
 export default pool
